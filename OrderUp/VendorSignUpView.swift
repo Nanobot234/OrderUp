b1b719a1d7c6411eb392c6ca8143177002e080e3
Nana Bonsu <Nbonsu2000@gmail.com>
2023-09-11 09:35:49 -0400

    ViewModel classes with a gitignore file

---
+++ OrderUp/VendorSignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  VendorSignUpView.swift
+//  OrderUp
+//
+//  Created by Nana Bonsu on 8/23/23.
+//
+
+import SwiftUI
+
+struct VendorSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VendorSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        VendorSignUpView()
+    }
+}
